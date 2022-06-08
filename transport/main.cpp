ecb40f83382779f6543db525f8e73dde0920be0c
EgorBroni <ebronitskiy123@gmail.com>
2022-06-08 17:13:55 +0300

    Initial Commit

---
+++ transport/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  transport
+//
+//  Created by Егор Броницкий on 08.06.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
